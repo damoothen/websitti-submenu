b3033ed18043f01493e8aac16130f5e3da976a9d
David Moothen <dmoothen@websitti.fr>
2015-11-24 03:42:43 +0100

    Calculate coordinates of submenu on button click.

--- src/js/jquery.submenu.js
+++ src/js/jquery.submenu.js
@@ -17,47 +17,48 @@
             var $menu = $('#' + id);
             var visible = false;
 
-            var menuWidth = $menu.outerWidth();
-            var menuOffset = $button.offset();
-
-            // Place menu after button in DOM.
-            $button.after($menu);
-
-            // If menu width is less than button width, set menu width to be equal to button width.
-            if ($menu.outerWidth() < $button.outerWidth()) {
-                menuWidth = $button.outerWidth();
-            }
-
-            // If menu width is more than screen width, set it to be equal to window width
-            else if (menuWidth > $window.width()) {
-                menuWidth = $window.width();
-                menuOffset.left = 0;
-            }
-
-            else {
-                if (align === 'right') {
-                    menuOffset.left = menuOffset.left - menuWidth + $button.outerWidth();
-                    if (menuOffset.left < 0) {
-                        menuOffset.left = 0;
-                    }
-                }
-
-                // If no align, menu is aligned to left. Check if menu does not get out of screen
-                else {
-                    if (menuOffset.left + $menu.outerWidth() > $window.width()) {
-                        menuOffset.left = $window.width() - $menu.outerWidth();
-                    }
-                }
-            }
-
-            $menu.width(menuWidth);
-            $menu.offset(menuOffset);
-
             // Display menu
             var showMenu = function(e){
                 e.preventDefault();
-                var $s = e.data;
+
                 if (!visible) {
+
+                    var menuWidth = $menu.outerWidth();
+                    var menuOffset = $button.offset();
+
+                    // Place menu after button in DOM.
+                    $button.after($menu);
+
+                    // If menu width is less than button width, set menu width to be equal to button width.
+                    if ($menu.outerWidth() < $button.outerWidth()) {
+                        menuWidth = $button.outerWidth();
+                    }
+
+                    // If menu width is more than screen width, set it to be equal to window width
+                    if (menuWidth > $window.width()) {
+                        menuWidth = $window.width();
+                        menuOffset.left = 0;
+                    }
+
+
+                    if (align === 'right') {
+                        menuOffset.left = menuOffset.left - menuWidth + $button.outerWidth();
+                        if (menuOffset.left < 0) {
+                            menuOffset.left = 0;
+                        }
+                    }
+
+                    // If no align, menu is aligned to left. Check if menu does not get out of screen
+                    else {
+                        if (menuOffset.left + $menu.outerWidth() > $window.width()) {
+                            menuOffset.left = $window.width() - $menu.outerWidth();
+                        }
+                    }
+
+                    $menu.width(menuWidth);
+                    $menu.offset(menuOffset);
+
+                    var $s = e.data;
                     $s.css({
                         'opacity': 0,
                         'visibility': 'visible'
